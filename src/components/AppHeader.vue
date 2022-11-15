8d8a1f2d7644a9e1ed40c8d377e7c4e33fbddd67
Stanghi <stangherlin.matti@gmail.com>
2022-11-15 15:13:17 +0100

    [init]

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppHeader',
+};
+</script>
+
+<template>
+    <header>H</header>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: 'AppMain',
+};
+</script>
+
+<template>
+    <main>M</main>
+</template>
+
+<style lang="scss" scoped></style>
